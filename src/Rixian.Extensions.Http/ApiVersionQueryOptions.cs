d9940a579f86c611b6e9fd66991950838cc50936
Samuel McAravey <mcaravey@rixian.net>
2019-09-14 14:09:22 -0700

    Added api version extension.

---
+++ src/Rixian.Extensions.Http/ApiVersionQueryOptions.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Rixian. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE file in the project root for full license information.
+
+namespace Rixian.Extensions.Http
+{
+    using System;
+    using System.Net.Http;
+    using System.Net.Http.Headers;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Rixian.Extensions.Tokens;
+
+    /// <summary>
+    /// Options for configuring an api version via the query string.
+    /// </summary>
+    public class ApiVersionQueryOptions
+    {
+        /// <summary>
+        /// Gets or sets the query parameter name to use.
+        /// </summary>
+        public string QueryParamName { get; set; } = "api-version";
+
+        /// <summary>
+        /// Gets or sets the api version value.
+        /// </summary>
+        public string Value { get; set; }
+    }
+}
---
+++ src/Rixian.Extensions.Http/ApiVersionQueryParamDelegatingHandler.cs
@@ -0,0 +1,46 @@
+﻿// Copyright (c) Rixian. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE file in the project root for full license information.
+
+namespace Rixian.Extensions.Http
+{
+    using System;
+    using System.Net.Http;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.Extensions.Options;
+
+    /// <summary>
+    /// Configures the Authentication header with the Bearer scheme and uses the AccessToken property of the ITokenClient.
+    /// </summary>
+    public class ApiVersionQueryParamDelegatingHandler : DelegatingHandler
+    {
+        private readonly ApiVersionQueryOptions options;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ApiVersionQueryParamDelegatingHandler"/> class.
+        /// </summary>
+        /// <param name="options">The options used to configure this handler.</param>
+        public ApiVersionQueryParamDelegatingHandler(IOptions<ApiVersionQueryOptions> options)
+        {
+            if (options is null)
+            {
+                throw new ArgumentNullException(nameof(options));
+            }
+
+            this.options = options.Value;
+        }
+
+        /// <inheritdoc/>
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            if (request is null)
+            {
+                throw new ArgumentNullException(nameof(request));
+            }
+
+            request.RequestUri = request.RequestUri.SetSingleQueryParam(this.options.QueryParamName ?? "api-version", this.options.Value);
+
+            return await base.SendAsync(request, cancellationToken).ConfigureAwait(false);
+        }
+    }
+}
